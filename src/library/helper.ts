90789d073de75aac1b8b6f9a2810f0bd896be89f
Himanshupal0001 <hpal7860@gmail.com>
2025-06-28 15:44:01 +0400

    fix: hot

--- src/library/helper.ts
+++ src/library/helper.ts
@@ -1,8 +1,8 @@
 export function formatDateToDMY(dateString: string): string {
   const date = new Date(dateString);
   const day = String(date.getUTCDate()).padStart(2, '0');
-  const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Months are zero-indexed
-  const year = String(date.getUTCFullYear()).slice(-2); // Get last 2 digits
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const year = String(date.getUTCFullYear()).slice(-2);
 
   return `${day}/${month}/${year}`;
 }
